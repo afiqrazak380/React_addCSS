43fadfd1f455dc12ea462cc77e534d2fff7d6324
AfiqRazak <afiqrazak380@gmail.com>
2023-11-09 12:02:20 +0000

    Inline Style method

---
+++ addCSS/src/Button.jsx
@@ -0,0 +1,22 @@
+
+
+
+function Button(){
+
+  const styles = {
+      backgroundColor: "bisque",
+      fontSize: "15px",
+      color: "black",
+      padding:"10px 20px",
+      borderRadius: "5px",
+      border:  "2px black solid",
+      cursor: "pointer",
+    
+  }
+
+  return(
+    <button style={styles}>Click me</button>
+  )
+}
+
+export default Button
